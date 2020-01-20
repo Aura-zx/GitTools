5a0db14aa75359c9a7e5cc7aa7c18b750488b6a5
zhouxin <kaomygod@gmail.com>
2020-01-20 21:23:59 +0800

    feat: test progress bar

---
+++ progressbar.js
@@ -0,0 +1,7 @@
+function renderProgressbar({ total, current }) {}
+
+const MOVE_LEFT = Buffer.from('1b5b3130303044', 'hex').toString();
+const MOVE_UP = Buffer.from('1b5b3141', 'hex').toString();
+
+console.log('hello a line');
+console.log(MOVE_UP + '1213');
